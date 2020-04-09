be9d2f7f244d69bdd0c0b780541ae773768f7aeb
julioalcantara <alcantara.juliosousa@gmail.com>
2020-04-09 17:30:59 +0100

    Authomatic Authentication

--- frontEnd/tgs-app/src/api/dataBase.js
+++ frontEnd/tgs-app/src/api/dataBase.js
@@ -1,6 +1,23 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
+const instance = axios.create({
     //start ngrok every 8 hours and update the Url => ngrok http 3000
-    baseURL: 'http://6f7f57c8.ngrok.io'
+    baseURL: 'http://0839adc6.ngrok.io'
 });
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+
+export default instance;
