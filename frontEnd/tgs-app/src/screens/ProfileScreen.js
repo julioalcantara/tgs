37103c0ddb923bfe8747b55ea1724148f92bf75a
julioalcantara <alcantara.juliosousa@gmail.com>
2020-04-09 20:19:06 +0100

    Fix

--- frontEnd/tgs-app/src/screens/ProfileScreen.js
+++ frontEnd/tgs-app/src/screens/ProfileScreen.js
@@ -1,41 +1,42 @@
 import React, { useContext } from 'react';
-import { StyleSheet, View, Text, SafeAreaView, ActivityIndicator } from 'react-native';
-import { Image, Button } from 'react-native-elements';
+import { StyleSheet, View, FlatList, TouchableOpacity } from 'react-native';
+import { Image, Button, ListItem } from 'react-native-elements';
+import { NavigationEvents } from 'react-navigation';
+
 import { Context as AuthContext } from '../context/AuthContext';
 import { Context as ProfileContext } from '../context/ProfileContext';
 
 const ProfileScreen = () => {
     const { signout } = useContext(AuthContext);
-    const {state: {currentUser}} = useContext(ProfileContext);
-
-    if(!currentUser)  {
-
-        <Button 
-            title= 'Sign out'
-            onPress={signout}
-        />
-        //spinner
-        //return <ActivityIndicator size="large" style={{marginTop: 200}} />
-    }
-
-    const name = currentUser.createdProfile.name;
-
+    const { state, fetchProfile } = useContext(ProfileContext);
+    //console.log(state);
     return (
-        <SafeAreaView  forceInset={{ top: 'always'}} style={styles.container}>
-            <View>
-                <Text>{name}</Text>
-            </View>
+        <View style={styles.container}>
+            <NavigationEvents onWillFocus = {fetchProfile}/>
             <View style={styles.imageStyle}>
                 <Image
                     source={ require('../images/logo2.png')}
                     style={{ width: 150, height: 150}}
                 />
             </View>   
+            <FlatList 
+                data = {state}
+                
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity>
+                            <ListItem chevron title={item._id} /> 
+                        </TouchableOpacity>
+                    );
+                    
+                }}
+                keyExtractor = {item => item._id}
+            />
             <Button 
                 title= 'Sign out'
                 onPress={signout}
             />
-        </SafeAreaView>
+        </View>
     );
 }
 
